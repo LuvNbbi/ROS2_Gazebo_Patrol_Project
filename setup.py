5113bc71d8b38d4dbc66dfe988bc5923571064bb
LuvNbbi <minsuu2@gmail.com>
2026-06-16 06:32:20 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'test_world_robot_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='minsu',
+    maintainer_email='minsu@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'test_world_robot_patrol = test_world_robot_control.test_world_robot_patrol:main'
+        ],
+    },
+)
